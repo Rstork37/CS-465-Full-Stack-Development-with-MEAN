8a68cd68b4e42807e5477b5212acbb8078118c19
Ryan Stork <Rstork37@gmail.com>
2021-07-18 18:18:50 -0500

    Express website, static HTML only

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
-/*GET travel view */
-const travel = (req, res) => {
-    res.render('travel',{title:'Travlr Getaways'});
+var fs = require('fs'); 
+
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/*GET travel view*/
+const travel = (req,res) => {
+    res.render('travel', {title: 'Travelr Getaways',trips});
 };
 
 module.exports = {
